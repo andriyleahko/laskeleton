218aa848fb1f290f1ff9f9d34e39892103c4d6a6
Andriy Leshko <andriyleshko@mail.ru>
2016-10-11 16:33:24 +0300

    impruve route system

--- config/routing/route.php
+++ config/routing/route.php
@@ -5,10 +5,30 @@
             'url' => '/my/first',
             'controller' => '\Controller\My\First',
             'method' => 'index',
-            'params' => 'a/b/c',
+            'params' => 'a',
             'role' => [
-                'denied' => ['Anonim'],
-                'allow' => []
+                'denied' => ['User'],
+                'allow' => ['Anonim']
+            ]
+        ],
+        'mysecond' => [
+            'url' => '/my/first',
+            'controller' => '\Controller\My\First',
+            'method' => 'index2',
+            'params' => 'a/b',
+            'role' => [
+                'denied' => ['User'],
+                'allow' => ['Anonim']
+            ]
+        ],
+        'mythird' => [
+            'url' => '/my/first',
+            'controller' => '\Controller\My\First',
+            'method' => 'index3',
+            #'params' => '',
+            'role' => [
+                'denied' => ['User'],
+                'allow' => ['Anonim']
             ]
         ],
         'root' => [
