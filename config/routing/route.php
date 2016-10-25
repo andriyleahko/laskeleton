60f57057f84ced35a8be30eb00435afe555ad9af
Andriy Leshko <andriyleshko@mail.ru>
2016-10-25 12:53:22 +0300

    exampl route

--- config/routing/route.php
+++ config/routing/route.php
@@ -7,8 +7,8 @@
             'method' => 'index',
             'params' => 'a',
             'role' => [
-                'denied' => ['User'],
-                'allow' => ['Anonim']
+                'denied' => ['Anonim'],
+                'allow' => ['User']
             ]
         ],
         'mysecond' => [
@@ -16,20 +16,12 @@
             'controller' => '\Controller\My\First',
             'method' => 'index2',
             'params' => 'a/b',
-            'role' => [
-                'denied' => ['User'],
-                'allow' => ['Anonim']
-            ]
+
         ],
         'mythird' => [
             'url' => '/my/first',
             'controller' => '\Controller\My\First',
             'method' => 'index3',
-            #'params' => '',
-            'role' => [
-                'denied' => ['User'],
-                'allow' => ['Anonim']
-            ]
         ],
         'root' => [
             'url' => '/',
